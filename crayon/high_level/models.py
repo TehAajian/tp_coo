930810c901e34703a73c27d4ab992b08bcd42f56
TehAajian <james.nguyen42@outlook.com>
2024-09-25 16:17:09 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -7,3 +7,14 @@ class Local(models.Model):
   surface = models.IntegerField()
   class Meta:
     abstract = True
+  def __str__(self):
+    return self.nom
+
+class Ville(models.Model):
+  nom = models.CharField(max_length=100)
+  code_postal = models.IntegerField()
+  prix_m_2 = models.IntegerField()
+  def __str__(self):
+    return self.nom
+
+  

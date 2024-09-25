5d8c7b61018e09ac9d0b88394b79e0782d83799b
TehAajian <james.nguyen42@outlook.com>
2024-09-25 16:09:00 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,17 +1,6 @@
 # election/models.py
 from django.db import models
 
-models.IntegerField()
-models.CharField(max_length=100)
-models.ManyToManyField(Machine)
-
-models.ForeignKey(
-Departement, # ou "self",
-on_delete=models.PROTECT,
-# blank=True, null=True,
-# related_name="+",
-)
-
 class Local(models.Model):
   nom = models.CharField(max_length=100)
   ville = models.ForeignKey(Ville, on_delete=models.CASCADE)

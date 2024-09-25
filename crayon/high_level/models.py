c2deed4c8d93a14f01891a75af2daa6bef1b7c78
TehAajian <james.nguyen42@outlook.com>
2024-09-25 16:39:15 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -7,7 +7,7 @@ class Ville(models.Model):
   prix_m_2 = models.IntegerField()
   
   def __str__(self):
-    return self.nom
+  return self.nom
 
 class Local(models.Model):
   nom = models.CharField(max_length=100)
@@ -15,10 +15,55 @@ class Local(models.Model):
   surface = models.IntegerField()
   class Meta:
     abstract = True
-    
   def __str__(self):
-    return self.nom
+  return self.nom
 
+class SiegeSocial(Local):
+  def __str__(self):
+  return self.nom
+
+class Usine(Local):
+  nom = models.CharField(max_length=100)
+  def __str__(self):
+  return self.nom
+      
+class Machine(Usine):
+  nom = models.CharField(max_length=100)
+  prix = models.IntegerField()
+  n_serie = models.IntegerField()
+  def __str__(self):
+  return self.nom
+
+class Stock(models.Model):
+  objet = models.CharField(max_length=100)
+  nombre = models.IntegerField()
+  def __str__(self):
+  return self.nom
+
+class Etape(models.Model):
+  nom = models.CharField(max_length=100)
+  machine = models.CharField(max_length=100)
+  quantite_ressource = models.IntegerField()
+  duree = models.IntegerField()
+  etape_suivante = models.CharField(max_length=100)
+  def __str__(self):
+  return self.nom
+
+class Objet(models.Model):
+  nom = models.CharField(max_length=100)
+  prix = models.IntegerField()
+  class Meta:
+    abstract = True
+  def __str__(self):
+  return self.nom
+
+class Ressource(Objet):
+  def __str__(self):
+  return self.nom
+
+class Produit(Objet):
+  def __str__(self):
+  return self.nom
 
 
   

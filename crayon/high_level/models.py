558b4198083e4e94e70bdaa7cf1948fd80d75bfd
TehAajian <james.nguyen42@outlook.com>
2024-09-25 14:42:11 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,1 +1,12 @@
-# Create your models here.
+# election/models.py
+from django.db import models
+class Election(models.Model):
+title = models.CharField(max_length=200)
+pub_date = models.DateTimeField()
+class Candidate(models.Model):
+election = models.ForeignKey(
+Election,
+on_delete=models.CASCADE,
+)
+name = models.CharField(max_length=200)
+votes = models.IntegerField(default=0)

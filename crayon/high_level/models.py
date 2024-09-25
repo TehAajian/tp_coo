ad4b30c2826905cc11a79f5afec0719fed55b020
TehAajian <james.nguyen42@outlook.com>
2024-09-25 15:34:53 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,23 +1,9 @@
 # election/models.py
 from django.db import models
-class Election(models.Model):
-title = models.CharField(max_length=200)
-pub_date = models.DateTimeField()
-class Candidate(models.Model):
-election = models.ForeignKey(
-Election,
-on_delete=models.CASCADE,
-)
-name = models.CharField(max_length=200)
-votes = models.IntegerField(default=0)
 
-class Local(models.model):
-  nom = models.CharField(max_length=100)
-  ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
-  surface = models.IntegerField()
-
-class Meta:
-  abstract = True
+models.IntegerField()
+models.CharField(max_length=100)
+models.ManyToManyField(Machine)
 
 models.ForeignKey(
 Departement, # ou "self",

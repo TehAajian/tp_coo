80147479a91df3bfec85f45c0d227e8904907c7b
TehAajian <james.nguyen42@outlook.com>
2024-09-25 15:38:58 +0200

    Update admin.py

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.IntegerField)
+admin.site.register(models.CharField)
+admin.site.register(models.ManyToManyField)
+admin.site.register(models.ForeignKey)

f4f8fe07739b434a1f31eb2f0eeb086c411e6b35
TehAajian <james.nguyen42@outlook.com>
2024-09-25 17:26:06 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Ville(models.Model):
-  nom_Ville = models.CharField(max_length=100)
+  nom = models.CharField(max_length=100)
   code_postal = models.IntegerField()
   prix_m_2 = models.IntegerField()
   def __str__(self):

bd61bfab6bbb968d70f34d69bf098012756cfd24
TehAajian <james.nguyen42@outlook.com>
2024-09-25 17:06:56 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -2,14 +2,14 @@
 from django.db import models
 
 class Ville(models.Model):
-  nom = models.CharField(max_length=100)
+  nom_Ville = models.CharField(max_length=100)
   code_postal = models.IntegerField()
   prix_m_2 = models.IntegerField()
   def __str__(self):
     return self.nom
 
 class Local(models.Model):
-  nom = models.CharField(max_length=100)
+  nom_Local = models.CharField(max_length=100)
   Ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
   surface = models.IntegerField()
   class Meta:
@@ -27,21 +27,21 @@ class Usine(Local):
     return self.nom
       
 class Machine(Usine):
-  nom = models.CharField(max_length=100)
-  prix = models.IntegerField()
+  nom_Machine = models.CharField(max_length=100)
+  prix_Machine = models.IntegerField()
   n_serie = models.IntegerField()
   def __str__(self):
     return self.nom
 
 class Stock(models.Model):
-  objet = models.CharField(max_length=100)
-  nombre = models.IntegerField()
+  objet_Stock = models.CharField(max_length=100)
+  nombre_Stock = models.IntegerField()
   def __str__(self):
     return self.nom
 
 class Etape(models.Model):
-  nom = models.CharField(max_length=100)
-  machine = models.CharField(max_length=100)
+  nom_Etape = models.CharField(max_length=100)
+  machine_Etape = models.CharField(max_length=100)
   quantite_ressource = models.IntegerField()
   duree = models.IntegerField()
   etape_suivante = models.CharField(max_length=100)
@@ -49,8 +49,8 @@ class Etape(models.Model):
     return self.nom
 
 class Objet(models.Model):
-  nom = models.CharField(max_length=100)
-  prix = models.IntegerField()
+  nom_Objet = models.CharField(max_length=100)
+  prix_Objet = models.IntegerField()
   class Meta:
     abstract = True
   def __str__(self):

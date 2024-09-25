4809a0108d4411f6a1c273389d1fa633a823ecd3
TehAajian <james.nguyen42@outlook.com>
2024-09-25 15:42:21 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -11,3 +11,11 @@ on_delete=models.PROTECT,
 # blank=True, null=True,
 # related_name="+",
 )
+
+class Local(models.Model):
+  nom = models.CharField(max_length=100)
+  ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
+  surface = models.IntegerField()
+  
+class Meta:
+  abstract = True

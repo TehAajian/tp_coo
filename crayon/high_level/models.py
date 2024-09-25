642cb3d290da58a26cd41249305884aaf2196beb
TehAajian <james.nguyen42@outlook.com>
2024-09-25 16:19:19 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,20 +1,22 @@
 # election/models.py
 from django.db import models
 
-class Local(models.Model):
-  nom = models.CharField(max_length=100)
-  ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
-  surface = models.IntegerField()
-  class Meta:
-    abstract = True
-  def __str__(self):
-    return self.nom
-
 class Ville(models.Model):
   nom = models.CharField(max_length=100)
   code_postal = models.IntegerField()
   prix_m_2 = models.IntegerField()
   def __str__(self):
     return self.nom
+
+class Local(models.Model):
+  nom = models.CharField(max_length=100)
+  Ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
+  surface = models.IntegerField()
+  class Meta:
+    abstract = True
+  def __str__(self):
+    return self.nom
+
+
 
   
